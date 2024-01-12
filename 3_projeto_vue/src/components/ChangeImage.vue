2e7f87ab7bc422d526b3d8566a60497699c9bb67
Diego <diegoalencar.jacober@gmail.com>
2024-01-12 20:58:27 -0300

    Emit

---
+++ 3_projeto_vue/src/components/ChangeImage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <button @click="$emit('changeImage')">Trocar imagem de perfil</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ChangeImage',
+    emits: ['changeImage']
+}
+</script>
+<style lang="">
+    
+</style>
--- 3_projeto_vue/src/components/Picture.vue
+++ 3_projeto_vue/src/components/Picture.vue
@@ -1,13 +1,22 @@
 <template>
     <img :src="avatar" :alt="descricao" height="120" width="120">
+    <ChangeImage @change-image="trocarImagem"/>
 </template>
 <script>
+import ChangeImage from './ChangeImage.vue';
+
 export default {
     name: 'Picture',
     data() {
         return {
             avatar: "https://avatars.githubusercontent.com/u/81656395?v=4",
             descricao: "Diego"
+        };
+    },
+    components: { ChangeImage },
+    methods: {
+        trocarImagem() {
+            this.avatar = this.avatar = "https://avatars.githubusercontent.com/u/156379818?v=4"
         }
     }
 }
