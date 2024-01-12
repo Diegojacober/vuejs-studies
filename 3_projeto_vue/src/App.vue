b3f08daac41011af55ec384d0a69310cbad98900
Diego <diegoalencar.jacober@gmail.com>
2024-01-12 20:11:06 -0300

    Css scoped e css global

--- 3_projeto_vue/src/App.vue
+++ 3_projeto_vue/src/App.vue
@@ -1,23 +1,37 @@
 <template>
-  <PrimeiroComponente/>
-  <LifeCycle/>
-  <Pessoa/>
-  <Form/>
+  <Header />
+  <PrimeiroComponente />
+  <LifeCycle />
+  <Pessoa />
+  <Form />
 </template>
 
 <script>
-  import PrimeiroComponente from './components/PrimeiroComponente.vue';
-  import LifeCycle from './components/LifeCycle.vue';
+import PrimeiroComponente from './components/PrimeiroComponente.vue';
+import LifeCycle from './components/LifeCycle.vue';
 import Pessoa from './components/Pessoa.vue';
 import Form from './components/Form.vue';
+import Header from './components/Header.vue';
 
-    export default {
-        name: 'App',
-        components: {
+export default {
+  name: 'App',
+  components: {
     PrimeiroComponente,
     LifeCycle,
     Pessoa,
-    Form
+    Form,
+    Header
+  }
 }
-    }
 </script>
+
+<style>
+body {
+  background-color: #333;
+  color: #fff;
+}
+
+a {
+  color: #f00;
+}
+</style>
---
+++ 3_projeto_vue/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+   <ul>
+    <li><a href="@">Imagens</a></li>
+    <li><a href="#">Home</a></li>
+    <li><a href="@">Contato</a></li>
+   </ul>
+</template>
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+<style scoped>
+    ul {
+        list-style: none;
+        display: flex;
+        background-color: #9999;
+    }
+
+    li{
+        margin-right: 10px;
+    }
+
+    li a {
+        color: rgb(87, 47, 122);
+    }
+</style>
