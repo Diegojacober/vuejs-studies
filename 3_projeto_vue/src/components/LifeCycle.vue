2d8251f545f9224a1b3dc40f84f00824de684194
Diego <diegoalencar.jacober@gmail.com>
2024-01-12 19:08:12 -0300

    Life cycle hooks

---
+++ 3_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h4>Meu nome é {{ name }}</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LifyCycle',
+        data() {
+            return {
+                name: 'n sei'
+            }
+        },
+        created() {
+            setTimeout(() => {
+                this.name = "Diego"
+            }, 1000)
+            
+        },
+        mounted() {
+            setTimeout(() => {
+                this.name = "Pedro"
+            }, 2000)
+        }
+    }
+</script>
