b4e157b025d24913716dcc5dbc37041fc03f8544
Diego <diegoalencar.jacober@gmail.com>
2024-01-12 19:31:07 -0300

    Conhecendo as diretivas

--- 3_projeto_vue/src/components/Info.vue
+++ 3_projeto_vue/src/components/Info.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
+        <p v-if="is_working">Estou trabalhando no momento.</p>
+        <p v-else>Não estou trabalhando no momento.</p>
+        <p v-show="show_email">diegoalencar.jacober@gmail.com</p>
         <p>Utilizo as seguintes tecnologias:</p>
         <ul>
             <li>javascript</li>
@@ -12,6 +14,12 @@
 
 <script>
 export default {
-    name: 'Info'
+    name: 'Info',
+    data() {
+        return {
+            is_working: true,
+            show_email: true
+        }
+    }
 }
 </script>
