18a3d8228f4581ac8fce1442f4a408589883c551
Diego <diegoalencar.jacober@gmail.com>
2024-01-12 18:50:16 -0300

    Dados em componentes

--- 3_projeto_vue/src/components/PrimeiroComponente.vue
+++ 3_projeto_vue/src/components/PrimeiroComponente.vue
@@ -1,9 +1,18 @@
 <template>
-    <h1>Olá vue!</h1>
+    <div>
+        <h1>Olá vue!</h1>
+        <p>Meu nome é {{ name }}</p>
+    </div>
 </template>
 
 <script>
-    export default {
-        name: 'Primeiro componente'
+export default {
+    name: 'Primeiro componente',
+    data() {
+        return {
+            // so podem ser usados nesse componente
+            name: "Diego"
+        }
     }
+}
 </script>
