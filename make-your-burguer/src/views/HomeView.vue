b2de6b7624c355f5a6abb97e3fe5ac36e2abfe9e
Diego <diegoalencar.jacober@gmail.com>
2024-01-13 13:32:35 -0300

    Banner

--- make-your-burguer/src/views/HomeView.vue
+++ make-your-burguer/src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <script setup>
+import Banner from '@/components/Banner.vue';
+
 </script>
 
 <template>
+  <Banner />
   <main class="main-container">
     <h1>Home</h1>
+    <p>formulário</p>
   </main>
 </template>
