ed60dab3a8c303b0cb26180e465605c69cab9206
Diego <diegoalencar.jacober@gmail.com>
2024-01-13 12:44:43 -0300

    Cabeçalho e rodapé

---
+++ make-your-burguer/src/views/NotFoundView.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <h1>Página não encontrada</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'NotFoundView'
+}
+</script>
+<style lang="">
+    
+</style>
