74ca8dda3eedbdaf95933c8f4008a6af4ff967a5
Diego <diegoalencar.jacober@gmail.com>
2024-01-16 22:43:23 -0300

    feat: Recuperando dados do banco

--- make-your-burguer/src/components/BurguerForm.vue
+++ make-your-burguer/src/components/BurguerForm.vue
@@ -10,21 +10,25 @@
                 <label for="bread">Escolha o pão:</label>
                 <select name="bread" id="bread" v-model="bread">
                     <option value="">Selecione seu pão</option>
-                    <option value="integral">integral</option>
+                    <option v-for="selectedBread in breads" :key="selectedBread.id" :value="selectedBread.tipo">
+                        {{ selectedBread.tipo }}
+                    </option>
                 </select>
             </div>
             <div class="input-container">
                 <label for="carne">Escolha a carne do seu Burguer:</label>
                 <select name="carne" id="carne" v-model="meat">
                     <option value="">Selecione sua carne</option>
-                    <option value="maminha">maminha</option>
+                    <option v-for="selectedMeat in breads" :key="selectedMeat.id" :value="selectedMeat.tipo">
+                        {{ selectedMeat.tipo }}
+                    </option>
                 </select>
             </div>
             <div id="optionals-container" class="input-container">
                 <label for="optionals" id="optionals-label">Selecione os opcionais:</label>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="optionals" v-model="optionals" value="Salame">
-                    <span>Salame</span>
+                <div class="checkbox-container" v-for="selectedOptional in optionalsData" :key="selectedOptional.id">
+                    <input type="checkbox" name="optionals" v-model="optionals" :value="selectedOptional.tipo">
+                    <span>{{ selectedOptional.tipo }}</span>
                 </div>
             </div>
             <div class="input-container">
@@ -35,72 +39,101 @@
 </template>
 <script>
 export default {
-    name: "BurguerForm"
+    name: "BurguerForm",
+    data() {
+        return {
+            breads: null,
+            meats: null,
+            optionalsData: null,
+            name: null,
+            bread: null,
+            meat: null,
+            optionals: [],
+            status: "Solicitado",
+            msg: null
+        }
+    },
+    methods: {
+        async getRecipes() {
+            const req = await fetch("http://localhost:3000/ingredientes");
+            const data = await req.json();
+
+            this.breads = data.paes;
+            this.meats = data.carnes;
+            this.optionalsData = data.opcionais;
+        }
+    },
+    mounted() {
+        this.getRecipes();
+    }
 }
 </script>
 <style scoped>
-    #burguer-form{
-        max-width: 400px;
-        margin: 0 auto;
-    }
-    .input-container {
-        display: flex;
-        flex-direction: column;
-        margin-bottom: 20px;
-    }
+#burguer-form {
+    max-width: 400px;
+    margin: 0 auto;
+}
 
-    label {
-        font-weight: bold;
-        margin-bottom: 15px;
-        color: #222;
-        padding: 5px 10px;
-        border-left: 4px solid #fcba03;
-    }
+.input-container {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+}
 
-    input, select {
-        padding: 5px 10px;
-        width: 300px;
-    }
+label {
+    font-weight: bold;
+    margin-bottom: 15px;
+    color: #222;
+    padding: 5px 10px;
+    border-left: 4px solid #fcba03;
+}
 
-    #optionals-container {
-        flex-direction: row;
-        flex-wrap: wrap;
-    }
+input,
+select {
+    padding: 5px 10px;
+    width: 300px;
+}
 
-    #optionals-label{
-        width: 100%;
-    }
+#optionals-container {
+    flex-direction: row;
+    flex-wrap: wrap;
+}
 
-    .checkbox-container {
-        display: flex;
-        align-items: flex-start;
-        width: 50%;
-        margin-bottom: 20px;
-    }
+#optionals-label {
+    width: 100%;
+}
 
-    .checkbox-container span, .checkbox-container input {
-        width: auto;
-    }
+.checkbox-container {
+    display: flex;
+    align-items: flex-start;
+    width: 50%;
+    margin-bottom: 20px;
+}
 
-    .checkbox-container span {
-        margin-left: 6px;
-        font-weight: bold;
-    }
+.checkbox-container span,
+.checkbox-container input {
+    width: auto;
+}
 
-    .submit-btn{
-        background: #222;
-        color: #fcba03;
-        font-weight: bold;
-        border: 2px solid #222;
-        padding: 10px;
-        font-size: 16px;
-        margin: 0 auto;
-        cursor: pointer;
-        transition: all ease-in .5s;
-    }
+.checkbox-container span {
+    margin-left: 6px;
+    font-weight: bold;
+}
 
-    .submit-btn:hover {
-        background: transparent;
-        color: #222;
-    }
+.submit-btn {
+    background: #222;
+    color: #fcba03;
+    font-weight: bold;
+    border: 2px solid #222;
+    padding: 10px;
+    font-size: 16px;
+    margin: 0 auto;
+    cursor: pointer;
+    transition: all ease-in .5s;
+}
+
+.submit-btn:hover {
+    background: transparent;
+    color: #222;
+}
 </style>
